d10d711242a256c37ae21e7052b9c78e781ae5ba
Truls Jørgensen <trulsjor@gmail.com>
2022-08-23 15:53:42 +0200

    THIS IS WHAT WINNING LOOKS LIKE
    Co-authored-by: Audun Fauchald Strand <audun.fauchald.strand@nav.no>

---
+++ src/main/kotlin/io/nais/plugins/Routing.kt
@@ -0,0 +1,26 @@
+package io.nais.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/data") {
+            println("called")
+            call.respondText("""
+                {"names": ["Truls", "Audun"]}
+            """.trimIndent(), ContentType.Application.Json)
+        }
+        // Static plugin. Try to access `/static/index.html`
+        static("/viz") {
+            resources("static")
+        }
+    }
+}
---
+++ src/test/kotlin/io/nais/ApplicationTest.kt
@@ -0,0 +1,28 @@
+package io.nais
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.http.content.*
+import io.ktor.server.webjars.*
+import java.time.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import io.nais.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
